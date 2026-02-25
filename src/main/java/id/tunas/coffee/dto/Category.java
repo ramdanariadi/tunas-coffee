22f277fa034f90e53328aafdef9d721bdecfd70b
Ramdan Ariadi <ramadanariadi@gmail.com>
2026-02-26 00:00:06 +0800

    feat: update dummy category images with new URLs

--- src/main/java/id/tunas/coffee/dto/Category.java
+++ src/main/java/id/tunas/coffee/dto/Category.java
@@ -13,12 +13,12 @@ public class Category {
 
     public static List<Category> createDummy(){
         return List.of(
-                new Category("Americano", "https://i.imgur.com/1111111.jpg"),
-                new Category("Cappuccino", "https://i.imgur.com/1111111.jpg"),
-                new Category("Flat White", "https://i.imgur.com/1111111.jpg"),
-                new Category("Raf", "https://i.imgur.com/1111111.jpg"),
-                new Category("Espresso", "https://i.imgur.com/1111111.jpg"),
-                new Category("Latte", "https://i.imgur.com/2222222.jpg"));
+                new Category("Americano", "https://i.ibb.co.com/FqcVFBTk/mug-coffee-PNG16825-1.png"),
+                new Category("Cappuccino", "https://i.ibb.co.com/qMt8MvRn/mug-coffee-PNG16828-1.png"),
+                new Category("Flat White", "https://i.ibb.co.com/YBR9y17K/mug-coffee-PNG16824-1.png"),
+                new Category("Raf", "https://i.ibb.co.com/FLxWHD1z/01-h-B4-380-380-removebg-preview-1.png"),
+                new Category("Espresso", "https://i.ibb.co.com/Q36hh4Dt/espresso-900x900-1600x1056-1-removebg-preview-1.png"),
+                new Category("Latte", "https://i.ibb.co.com/Dfwz27M3/mug-coffee-PNG16863-1.png"));
     }
 
     public String getName() {
