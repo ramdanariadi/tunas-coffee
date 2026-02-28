0c633b146eb32e49efb036c91420ea1b31e18524
Ramdan Ariadi <ramadanariadi@gmail.com>
2026-02-28 21:49:26 +0700

    feat: add time picker functionality and update order layout with new UI elements

--- src/main/java/id/tunas/coffee/OrderActivity.java
+++ src/main/java/id/tunas/coffee/OrderActivity.java
@@ -1,9 +1,12 @@
 package id.tunas.coffee;
 
 import android.os.Bundle;
+import android.widget.TextClock;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -11,5 +14,15 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        TextClock textClock = findViewById(R.id.textClock);
+        textClock.setOnClickListener(view -> {
+            MaterialTimePicker timePicker = new MaterialTimePicker.Builder()
+                    .setTimeFormat(TimeFormat.CLOCK_24H)
+                    .setTitleText("Chose Time")
+                    .build();
+
+            timePicker.show(getSupportFragmentManager(), "TIME_PICKER");
+        });
     }
 }
