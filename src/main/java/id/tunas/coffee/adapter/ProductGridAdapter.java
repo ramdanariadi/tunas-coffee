d2161b8c029a76a0dad127d3ca9437f693acc2f5
Ramdan Ariadi <ramadanariadi@gmail.com>
2026-02-28 10:29:02 +0800

    feat: implement ProductGridAdapter and remove category layout

---
+++ src/main/java/id/tunas/coffee/adapter/ProductGridAdapter.java
@@ -0,0 +1,58 @@
+package id.tunas.coffee.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import id.tunas.coffee.R;
+import id.tunas.coffee.dto.Product;
+
+public class ProductGridAdapter extends RecyclerView.Adapter<ProductGridAdapter.ProductViewHolder> {
+
+    private final List<Product> products;
+
+    public ProductGridAdapter(List<Product> categories) {
+        this.products = categories;
+    }
+
+    @NonNull
+    @Override
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.product_grid_item, parent,false);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
+        Product category = products.get(position);
+        holder.product.setText(category.getName());
+        Glide.with(holder.itemView.getContext()).load(category.getImg()).into(holder.img);
+    }
+
+    @Override
+    public int getItemCount() {
+        return products.size();
+    }
+
+    public static class ProductViewHolder extends RecyclerView.ViewHolder {
+        private final TextView product;
+        private final ImageView img;
+
+        public ProductViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            this.product = itemView.findViewById(R.id.category_text);
+            this.img = itemView.findViewById(R.id.category_image);
+        }
+    }
+}
